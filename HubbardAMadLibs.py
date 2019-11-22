5c05e964133866b3d38752101778e659eef6f612
ah01557n <58082340+ah01557n@users.noreply.github.com>
2019-11-22 10:47:39 -0500

    Add files via upload

---
+++ HubbardAMadLibs.py
@@ -0,0 +1,11 @@
+answer = input("What is your favorite food?")
+
+answer2 = input("Favorite school subject")
+
+answer3 = input("Pick an article of clothing")
+
+answer4 = input("A room in your home")
+
+answer5 = input("Favorite color")
+
+print("One day I was eating" + answer + "and I realized I forgot my" + answer5 + answer3 + "in my" + answer2 + "class. I thought it was in my " + answer4 + "room, but I left it at school in my" + answer2 + "class because there was" + answer + "on it.") 
